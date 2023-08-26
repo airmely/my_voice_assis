17d930c18c4527b9fa39fadb64803e57264798ae
Мелконян Айреник Альбертович <114315967+airmely@users.noreply.github.com>
2023-08-26 20:03:17 +0300

    Delete mytest.py

--- mytest.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-
-text = "Привет! Я ваш голосовой помощник."
-tts = gTTS(text, lang="ru", slow=False)  # Здесь вы можете настроить язык и скорость
-tts.save("output.mp3")  # Сохраняем сгенерированный голос в файл
